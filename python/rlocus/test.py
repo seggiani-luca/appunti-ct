23fba81ab07a0184a64336d258bc42986a9d273f
Luca Seggiani <seggianiluca2004@gmail.com>
2025-04-24 19:41:03 +0200

    Lezione di oggi

--- python/rlocus/test.py
+++ python/rlocus/test.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import control as ctrl
 
-num = [1, 5]
-den = [1, 6, 109, 0]
+num = [1, 10]
+den = [1, 2, 17, 16, 64, 0]
 sys = ctrl.TransferFunction(num, den)
 
 ctrl.root_locus_plot(sys, plot=True, title="")
 plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
 
-plt.xlim([-10, 1])
+plt.xlim([-20, 20])
+plt.ylim([-20, 20])
 
 plt.show()
