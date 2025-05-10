1ca1f00387fe8d03266ac5e47b5ebac1b1ee3889
Luca Seggiani <seggianiluca2004@gmail.com>
2025-05-11 01:42:58 +0200

    Ultimi appunti parzialmente rivisti

--- python/margin.py
+++ python/margin.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import control as ctrl
 
-num = [400 * x for x in [0.01, 0.2, 1]]
-den = [1, 1, 0, 0]
+num = [10, 0]
+den = [1, 2, 1]
 sys = ctrl.TransferFunction(num, den)
 
-ctrl.bode_plot(sys, margins=True, title="")
+ctrl.bode_plot(sys, title="")
 
 plt.show()
--- python/nyquist.py
+++ python/nyquist.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import control as ctrl
 
-num = [400 * x for x in [0.01, 0.2, 1]]
-den = [1, 1, 0, 0]
+num = [10, 0]
+den = [1, 2, 1]
 sys = ctrl.TransferFunction(num, den)
 
-ctrl.nyquist_plot(sys, title="", max_curve_magnitude=10)
+ctrl.nyquist_plot(sys, title="", max_curve_magnitude=10, unit_circle=True)
 
 plt.show()
