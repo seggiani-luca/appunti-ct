4fab1e1bc30337d415befc3c20b3f6d1b5163cc3
Luca Seggiani <seggianiluca2004@gmail.com>
2025-05-13 00:19:56 +0200

    Ultime lezioni riviste

--- python/margin.py
+++ python/margin.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import control as ctrl
 
-num = [10, 0]
-den = [1, 2, 1]
+num = [1, 0.5]
+den = [1, 0]
 sys = ctrl.TransferFunction(num, den)
 
-ctrl.bode_plot(sys, title="")
+ctrl.bode_plot(sys, margins=True, title="")
 
 plt.show()
